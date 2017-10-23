13fc32614a63fabcfb5ade1352d681b34c0eef18
lovelyalchemist <ebruesra06@gmail.com>
2017-10-23 21:11:18 +0300

    Update app.js

--- V-for/app.js
+++ V-for/app.js
@@ -1,4 +1,4 @@
-window.addEventListener('load',() => {
+window.addEventListener('load', () => {
   window.vue = new Vue({
     el: '#app',
     data: {
@@ -8,22 +8,32 @@ window.addEventListener('load',() => {
     },
     methods: {
       removeFromCart(index) {
-        this.cart.splice(index, 1);
-      },
-      saveForLater(index) {
-        const item = this.cart.splice(index,1);
-        console.log(item);
-        this.saved.push(item[0]);
-      }
+          this.cart.splice(index, 1);
+        },
+        saveForLater(index) {
+          const item = this.cart.splice(index, 1);
+          console.log(item);
+          this.saved.push(item[0]);
+        },
+        removeFromSavedList(index) {
+        this.saved.splice(index);
+        },
+         moveToCart(index) {
+          const item = this.saved.splice(index, 1);
+          this.cart.push(item[0]);
+        }
+        
     },
     created() {
-      fetch('./data.json')
-        .then((res) => { return res.json() })
-        .then((res) => { 
+      fetch('https://raw.githubusercontent.com/lovelyalchemist/Vue/master/V-for/data.json')
+        .then((res) => {
+          return res.json()
+        })
+        .then((res) => {
           this.isLoading = false;
           this.cart = res.cart;
           this.saved = res.saved;
-      })
+        })
 
     }
   })
