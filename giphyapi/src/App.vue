4240cb4b34496acb3dcd66548f729ada5173b04d
lovelyalchemist <ebruesra06@gmail.com>
2017-10-26 19:55:19 +0300

    giphy api

--- giphyapi/src/App.vue
+++ giphyapi/src/App.vue
@@ -1,16 +1,48 @@
 <template>
   <div id="app">
-    <search></search>
-    
+    <search v-on:SearchRequested="handleSearch"></search>
+    <p v-if="isLoading"><b> It's Loading.Please Wait.</b></p>
+    <preview :gifs=gifs></preview>
   </div>
 </template>
 
 <script>
-import search from '.components/search.vue'
+import search from './components/search.vue'
+import preview from './components/preview.vue'
+
 
 export default {
   name: 'app',
-  components: { search}
+  components: { 
+    'search': search, 
+    'preview': preview
+  },
+  data() {
+    return {
+      isLoading: true,
+      gifs: []
+    }
+  },
+  methods: {
+    doQuery(url) {
+    fetch(url)
+    .then((res) => { return res.json() } )
+        .then((res) => { 
+          this.gifs = res.data;
+          this.isLoading = false;
+        })
+  },
+    handleSearch(query) {
+      this.gifs = [];
+      this.isLoading = true;
+      const url = (`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=sdjbcCwbsIcxcjSAJGlUOpH5mdfJWVrV`)
+      this.doQuery(url)
+  }
+},
+  created() {
+    const url = ('http://api.giphy.com/v1/gifs/trending?api_key=sdjbcCwbsIcxcjSAJGlUOpH5mdfJWVrV')
+    this.doQuery(url)
+  }
 }
 </script>
 
@@ -24,7 +56,6 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-
 
 a {
   color: #42b983;
