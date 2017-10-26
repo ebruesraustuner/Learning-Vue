1b1701fb72210fdf877a5f974488df4365e59a1c
lovelyalchemist <ebruesra06@gmail.com>
2017-10-26 13:18:35 +0300

    Giphy Api

---
+++ giphyapi/components/search.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="search">
+		<input type="text" v-model="query" />
+		<button v-on:click="handleSearch"> Search </button>
+	</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			query: ''
+		}
+	},
+	methods: {
+		handleSearch() {
+			console.log(this.query);
+		}
+	}
+}
+</script>
---
+++ giphyapi/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <search></search>
+    
+  </div>
+</template>
+
+<script>
+import search from '.components/search.vue'
+
+export default {
+  name: 'app',
+  components: { search}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+
+a {
+  color: #42b983;
+}
+</style>
